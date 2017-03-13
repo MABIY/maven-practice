24e4088449cb329ec7e8d5514916dd0c78d7acce
MABIY <jyfc7879@gmail.com>
2017-03-13 10:11:56 +0800

    持久话 serialVersionUID修改

--- account-email/src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailException.java
+++ account-email/src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailException.java
@@ -4,7 +4,7 @@ package com.juvenxu.mvnbook.account.email;
  * Created by lh on 17-2-28.
  */
 public class AccountEmailException extends Exception {
-    public static final long servialVersionUID = -4817386460334501672L;
+    public static final long serialVersionUID = -4817386460334501672L;
 
     public AccountEmailException(String message) {
         super(message);
